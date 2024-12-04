7f4a47c8e7649eeaaf5ae258d648ea9563c85d96
Ryan Sun <29398810+RyanSun-dev@users.noreply.github.com>
2024-12-03 21:58:03 -0800

    Add files via upload

--- skipList_c++/skiplist.cpp
+++ skipList_c++/skiplist.cpp
@@ -2,8 +2,15 @@
 #include "skiplist.hpp"
 #include <iostream>
 
+bool coin_toss(){
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<int> dis(0,1);
+    return dis(gen) == 1?true:false;
+}
+
 skipList::skipList(){
-    head = new Node(0, maxNumberOfLevel);
+    head = new Node(0, maxNumberOfLevel,std::chrono::system_clock::now());
     level = 0;
 }
 
@@ -11,13 +18,13 @@ void skipList::insert(int data){
     int newLevel = 0;
 
     // random coin toss
-    while(newLevel < maxNumberOfLevel and rand()%2 ==1){
+    while(newLevel < maxNumberOfLevel and coin_toss()){
         newLevel++;
     }
 
     // increase the level of skiplist if more than previous highest level
     if(newLevel > level){
-        head->next.resize(newLevel + 1, nullptr);
+        head->forward.resize(newLevel + 1, nullptr);
         level = newLevel;
     }
 
@@ -25,42 +32,43 @@ void skipList::insert(int data){
     Node* current = head;
     std::vector<Node*> update(level+1,nullptr);
     for(int i = level; i >= 0;i--){
-        while(current->next[i] and current->next[i]->data < data){
-            current = current->next[i];
+        while(current->forward[i] and current->forward[i]->data < data){
+            current = current->forward[i];
         }
         update[i] = current;
     }
 
     // insert the new data
-    current = current->next[0];
+    current = current->forward[0];
     if(current == nullptr or current->data != data){
-        Node* newNode = new Node(data,newLevel);
+        std::chrono::system_clock::time_point timestamp = std::chrono::system_clock::now();
+        Node* newNode = new Node(data,newLevel,timestamp);
         for(int i =0;i <= newLevel;i++){
-            newNode->next[i] = update[i]->next[i];
-            update[i]->next[i] = newNode;
+            newNode->forward[i] = update[i]->forward[i];
+            update[i]->forward[i] = newNode;
         }
 
-        std::cout<<"Element "<<data<<" added successfully!"<<std::endl;
+        //std::cout<<"Element "<<data<<" added successfully!"<<std::endl;
     } else{
-        std::cout<<"Element "<<data<<" already exists!"<<std::endl;
+        //std::cout<<"Element "<<data<<" already exists!"<<std::endl;
     }
 
 }
 
-bool skipList::search(int data){
+int skipList::search(int data){
 
     Node* current = head;
 
     // start at the highest level
     for(int i = level; i >= 0; i--){
-        while(current->next[i]){
-            if(current->data == data) return true;
-            if(current->next[i]->data > data) break;
-            current = current->next[i];
+        while(current->forward[i]){
+            if(current->data == data) return data;
+            if(current->forward[i]->data > data) break;
+            current = current->forward[i];
         }
     }
 
-    return false;
+    return -1;
 
 }
 
@@ -68,44 +76,47 @@ void skipList::display(){
     std::cout<<"SkipList: "<<std::endl;
 
     for(int i = level;i >= 0; i--){
-        Node* current = head->next[i];
+        Node* current = head->forward[i];
 
         std::cout<<"Level "<<i<<":   ";
         std::cout<<"Head->";
         while(current != nullptr){
-            if(current->next[i] != nullptr){
+            if(current->forward[i] != nullptr){
                 std::cout<<current->data<<"->";
             } else{
                 std::cout<<current->data<<"->tail";
             }
-            current = current->next[i];
+            current = current->forward[i];
         }
         std::cout<<std::endl;
     }
 }
 
-void skipList::remove(int data){
+bool skipList::remove(int data){
     Node* current = head;
 
     std::vector<Node*> update(level+1,nullptr);
     for(int i = level; i >= 0; i--){
-        while(current->next[i] and current->next[i]->data < data){
-            current = current->next[i];
+        while(current->forward[i] and current->forward[i]->data < data){
+            current = current->forward[i];
         }
         update[i] = current;
     }
 
-    current = current->next[0];
+    current = current->forward[0];
     if(current->data == data){
         for(int i = 0;i <= level; i++){
-            if(update[i]->next[i] != current) break;
-            update[i]->next[i] = current->next[i];
+            if(update[i]->forward[i] != current) break;
+            update[i]->forward[i] = current->forward[i];
         }
         delete current;
-        std::cout<<"Element "<<data<<" deleted successfully!!"<<std::endl;
+        //std::cout<<"Element "<<data<<" deleted successfully!!"<<std::endl;
 
-        while(level > 0 and head->next[level] == nullptr){
+        while(level > 0 and head->forward[level] == nullptr){
             level--;
         }
-    }  
+        return true;
+    }
+
+    return false;
 }
--- skipList_c++/skiplist.hpp
+++ skipList_c++/skiplist.hpp
@@ -1,12 +1,15 @@
 #include <vector>
+#include <chrono>
+#include <optional>
 
 const int maxNumberOfLevel = 16;
 
 class Node{
 public:
     int data;
-    std::vector<Node*> next;
-    Node(int data, int level) : data(data), next(level +1, nullptr) {};
+    std::chrono::system_clock::time_point timestamp;
+    std::vector<Node*> forward;
+    Node(int data, int level, std::chrono::system_clock::time_point timestamp) : data(data), forward(level +1, nullptr),timestamp(timestamp) {};
 };
 
 class skipList{
@@ -18,7 +21,7 @@ public:
     skipList();
 
     void insert(int data);
-    void remove(int data);
-    bool search(int data);
+    bool remove(int data);
+    int search(int data);
     void display();
 };
